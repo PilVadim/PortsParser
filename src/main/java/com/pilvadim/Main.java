e726773f18dfb19efd9e81ae0b3d8a1b9d7b48b9
vadimpilipenko <pilvadim@gmail.com>
2022-06-21 19:34:05 +0300

    added manual input for main

--- src/main/java/com/pilvadim/Main.java
+++ src/main/java/com/pilvadim/Main.java
@@ -1,6 +1,7 @@
 package main.java.com.pilvadim;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 import static main.java.com.pilvadim.parsers.OptionsChecker.listAllOptions;
 import static main.java.com.pilvadim.parsers.Parser.parse;
@@ -8,10 +9,25 @@ import static main.java.com.pilvadim.parsers.Parser.parse;
 public class Main {
 
     public static void main(String[] args) {
-        String[] input = {"1,3-5", "2", "3-4"};
-        int[][] result = listAllOptions( parse( input ) );
+
+        String[] inputArr = {"1,3-5", "2", "3-4"};
+
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+
+        if ( ! input.isEmpty() )
+            inputArr = input.split(", ");
+
+        for (int i = 0; i < inputArr.length; i++)
+            inputArr[i] = inputArr[i].replace("\"","");
+
+        s.close();
+
+        int[][] result = listAllOptions( parse( inputArr ) );
+
         for (int[] r:result)
             System.out.println(Arrays.toString(r));
+
     }
 
 }
